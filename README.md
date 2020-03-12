79b61969b95c9a6fa538809e60335d78ad0dd92e
Desislava Stoyanova <dstoyanova929@gmail.com>
2020-03-12 18:12:14 +0100

    Added instructions and a sample file for report generation

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # cifar-example-project
+
+## Custom generation of Reports
+### Prerequisites
+
+* Login into your MiniO instance and make sure you have 3 buckets available:
+    * models
+    * reports
+    * dataset
+
+* Upload a *test.csv* validation data set into your **dataset** bucket.
+
+* In Scaleout Studio, add a Model under Models and get the UID which has been assigned to that model 
+(use our API for that).
+
+* Upload a sequential model into your **models** bucket and make sure the name of the model is the UID assigned to the
+model in Scaleout Studio.
+
+* Change the endpoint, access_key and secret_key (in *default.py*) to be accurate for your Project's MiniO instance:
+
+```python
+minioClient = Minio('minio.generators.platform.demo.scaleout.se',
+                    access_key='GXfzlBNHX2dALokXArRm',
+                    secret_key='yI5pGGjgLBB62JOeCYMOHrsTOH9C2mYp1NhkB9r2',
+                    secure=False)
+```
+
+* Create a new report generator for your Project under Reports in Scaleout Studio. Use the default generator 
+*default.py* as a starting point.
+
+* Go back to Models in Scaleout Studio, click Reports for a specific model and select the *default.py* as a generator.
+
+* You are ready to generate your custom reports.
---
+++ report_generators/default.py
@@ -0,0 +1,51 @@
+import sys
+import numpy
+from minio import Minio
+from sklearn.metrics import classification_report
+
+minioClient = Minio('minio.generators.platform.demo.scaleout.se',
+                    access_key='GXfzlBNHX2dALokXArRm',
+                    secret_key='yI5pGGjgLBB62JOeCYMOHrsTOH9C2mYp1NhkB9r2',
+                    secure=False)
+
+
+def read_test_data():
+    import pandas
+
+    data = minioClient.get_object('dataset', 'test.csv')
+    with open('media/datasets/test.csv', 'wb') as file_data:
+        for d in data.stream(32 * 1024):
+            file_data.write(d)
+
+    test_data = numpy.array(pandas.read_csv("media/datasets/test.csv"))
+
+    X_validate = test_data[:, 1::]
+    y_validate = test_data[:, 0]
+    classes = range(10)
+
+    return X_validate, y_validate, classes
+
+
+def generate_report(model_uid):
+    import pickle
+
+    data = minioClient.get_object('models', model_uid)
+    model = pickle.loads(data.read())
+
+    img_rows, img_cols = 28, 28
+    x_test, y_test, classes = read_test_data()
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.astype('float32')
+    x_test /= 255
+    y_predict = model.predict_classes(x_test)
+    report = classification_report(y_test, y_predict)
+
+    return report
+
+
+if __name__ == '__main__':
+    model_uid = str(sys.argv[1])
+
+    report = generate_report(model_uid)
+
+    print(report)
